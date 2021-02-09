7dd4ea06ecf0567591e6420820e2a43e78b78f1e
lppgn <33622086+lppgn@users.noreply.github.com>
2021-02-09 12:19:34 +0100

    First Commit

---
+++ superheroAcademy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace superheroAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
